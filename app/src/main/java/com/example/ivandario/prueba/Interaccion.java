bff07959bee148a0db6727093b4728974adb5b1a
julioprofe <julio_15.97@hotmail.com>
2017-05-25 15:53:25 -0500

    asa

--- app/src/main/java/com/example/ivandario/prueba/Interaccion.java
+++ app/src/main/java/com/example/ivandario/prueba/Interaccion.java
@@ -7,12 +7,14 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.Observable;
 import java.util.Observer;
 
 public class Interaccion extends AppCompatActivity implements SensorEventListener, Observer{
     private SensorManager mSensor;
+    private TextView xd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +22,14 @@ public class Interaccion extends AppCompatActivity implements SensorEventListene
         setContentView(R.layout.activity_interaccion);
 
         mSensor = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        xd = (TextView) findViewById(R.id.x);
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        float x = sensorEvent.values[0];
+        //float x = sensorEvent.values[0];
 
-
+        xd.setText(Float.toString(sensorEvent.values[0]));
     }
 
     @Override
